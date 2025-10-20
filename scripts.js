866a3b790bedc34f69b1f681d8a423e777f84b44
Th3Catalyst <max675295@gmail.com>
2025-10-20 15:45:11 -0700

    h

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
+board = [];
+for (let j = 0; j<10; j++) {
+  row = [];
 for (let i = 0; i<10;i++) {
-  a = document.createElement(div);
+  a = document.createElement("div");
   a.classList.add("square");
-  a.style.top=10;
-  a.style.left=10+40*i;
+  a.style.top=`${10+40*j}px`;
+  a.style.left=`${10+40*i}px`;
+  document.body.appendChild(a);
+  a.addEventListener("click", () => {
+    board[j][i].style.backgroundColor = "yellow";
+  });
+  row.push(a);
+}
+board.push(row);
+}
+console.log(board);
+
+for (let i = 0; i<20; i++) {
+  
+}
